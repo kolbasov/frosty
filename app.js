e0eed811da8d4759826a47f5c1c0dcdc5a01c1b3
Andrey Kolbasov <andrey.kolbasov@gmail.com>
2012-12-01 14:02:39 +0700

    HTTPS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
+	, fs = require('fs')
 	,	http = require('http')
+	,	https = require('https')
 	,	path = require('path')
 	,	routes = require('./routes');
 
@@ -18,5 +20,19 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', routes.index);
 frosty.setup(app);
 
-app.listen(8080);
-console.log('listening on port 8080');
+/*
+	Use these commands to generate SSL certificate.
+	openssl genrsa -out frosty_key.pem 1024
+	openssl req -new -key frosty_key.pem -out frosty_csr.pem
+	openssl x509 -req -in frosty_csr.pem -signkey frosty_key.pem -out frosty_cert.pem
+*/
+var httpsOptions = {
+	key: fs.readFileSync(process.env.FROSTY_HTTPS_KEY || 'frosty_key.pem'),
+	cert: fs.readFileSync(process.env.FROSTY_HTTPS_CERT || 'frosty_cert.pem')
+};
+
+// Lets use both HTTP and HTTPS.
+http.createServer(app).listen(8080);
+https.createServer(httpsOptions, app).listen(43443);
+
+console.log('listening on ports 8080 and 43443');
